878ac081c7facebfbcdd01cce155f6972829dd69
Võ Thiên Tài <thientai015344@gmail.com>
2024-04-12 14:12:21 +0700

    'first_commit'

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
 onload = () =>{
         document.body.classList.remove("container");
 };
+
+
+function playAudios() { 
+        var btn = document.getElementById("btncl")
+        btn.style="display: none"
+        var x = document.getElementById("myAudio"); 
+        x.play(); 
+}
